69574369c4e156ec3142b4b1f4f0b470f00a032b
Alan Davis <alan.davis@alfresco.com>
2018-02-23 11:59:34 +0000

    MNT-19261/REPO-3324 ISO8601DateFormat now handles Julian dates

--- src/main/java/org/alfresco/util/ISO8601DateFormat.java
+++ src/main/java/org/alfresco/util/ISO8601DateFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2010 Alfresco Software Limited.
+ * Copyright (C) 2005-2016 Alfresco Software Limited.
  *
  * This file is part of Alfresco
  *
@@ -25,10 +25,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
-import org.alfresco.api.AlfrescoPublicApi;  
+import org.alfresco.api.AlfrescoPublicApi;
 import org.alfresco.error.AlfrescoRuntimeException;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
+import org.joda.time.*;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 
 /**
@@ -38,9 +40,9 @@ import org.joda.time.DateTimeZone;
  * </pre>
  * where:
  * <ul>
- *   <li>sYYYY  Four-digit year with optional leading positive (<b>+</b>) or negative (<b>-</b>) sign. 
- *          A negative sign indicates a year BCE. The absence of a sign or the presence of a 
- *          positive sign indicates a year CE (for example, -0055 would indicate the year 55 BCE, 
+ *   <li>sYYYY  Four-digit year with optional leading positive (<b>+</b>) or negative (<b>-</b>) sign.
+ *          A negative sign indicates a year BCE. The absence of a sign or the presence of a
+ *          positive sign indicates a year CE (for example, -0055 would indicate the year 55 BCE,
  *          while +1969 and 1969 indicate the year 1969 CE).</li>
  *   <li>MM     Two-digit month (01 = January, etc.)</li>
  *   <li>DD     Two-digit day of month (01 through 31)</li>
@@ -54,36 +56,48 @@ import org.joda.time.DateTimeZone;
 public class ISO8601DateFormat
 {
     private static ThreadLocal<Map<TimeZone, Calendar>> calendarThreadLocal = new ThreadLocal<Map<TimeZone, Calendar>>();
+    public static final TimeZone UTC_TIMEZONE = TimeZone.getTimeZone("UTC");
+
     /**
      * Get a calendar object from cache.
+     * @param timezone timezone object to indicate the timezone to be used by the returned calendar object
      * @return calendar object from cache or newly created (if cache is empty)
      */
-    public static Calendar getCalendar()
+    public static Calendar getCalendar(TimeZone timezone)
     {
         if (calendarThreadLocal.get() == null)
         {
             calendarThreadLocal.set(new HashMap<TimeZone, Calendar>());
         }
-        
-        Calendar calendar = calendarThreadLocal.get().get(TimeZone.getDefault());
+
+        Calendar calendar = calendarThreadLocal.get().get(timezone);
         if (calendar == null)
         {
-            calendar = new GregorianCalendar();
-            calendarThreadLocal.get().put(TimeZone.getDefault(), calendar);
+            calendar = new GregorianCalendar(timezone);
+            calendarThreadLocal.get().put(timezone, calendar);
         }
-        
+
         return calendar;
     }
-    
+
+    /**
+     * Get a calendar object from cache for the system default timezone.
+     * @return calendar object from cache or newly created (if cache is empty)
+     */
+    public static Calendar getCalendar()
+    {
+        return getCalendar(TimeZone.getDefault());
+    }
+
     /**
      * Format date into ISO format (UCT0 / Zulu)
-     * 
+     *
      * @param isoDate  the date to format
      * @return  the ISO Zulu timezone formatted string
      */
-     public static String format(Date isoDate)
-     {
-        Calendar calendar = getCalendar();
+    public static String format(Date isoDate)
+    {
+        Calendar calendar = getCalendar(UTC_TIMEZONE);
         calendar.setTime(isoDate);
 
         // MNT-9790
@@ -130,61 +144,48 @@ public class ISO8601DateFormat
                 formatted.append(val);
             }
 
-            TimeZone tz = calendar.getTimeZone();
-            int offset = tz.getOffset(calendar.getTimeInMillis());
-            if (offset != 0)
-            {
-                int hours = Math.abs((offset / (60 * 1000)) / 60);
-                int minutes = Math.abs((offset / (60 * 1000)) % 60);
-                formatted.append(offset < 0 ? '-' : '+');
-                formatted.append(hours < 10 ? ("0" + hours) : hours);
-                formatted.append(':');
-                formatted.append(minutes < 10 ? ("0" + minutes) : minutes);
-            }
-            else
-            {
-                formatted.append('Z');
-            }
+            // ALF-21965 We are now confident we are using UTC timezone with zero offset
+            formatted.append('Z');
 
             return formatted.toString();
         }
     }
-   
+
     /**
      * Normalise isoDate time to Zulu(UTC0) time-zone, removing any UTC offset.
      * @param isoDate
-     * @return the ISO Zulu timezone formatted string 
-     * 			e.g 2011-02-04T17:13:14.000+01:00 -> 2011-02-04T16:13:14.000Z
+     * @return the ISO Zulu timezone formatted string
+     *             e.g 2011-02-04T17:13:14.000+01:00 -> 2011-02-04T16:13:14.000Z
      */
     public static String formatToZulu(String isoDate)
     {
-		try 
-		{
-			DateTime dt = new DateTime(isoDate, DateTimeZone.UTC);
-			return dt.toString();
-		} catch (IllegalArgumentException e) 
-		{
-			throw new AlfrescoRuntimeException("Failed to parse date " + isoDate, e);
-		}
+        try
+        {
+            DateTime dt = new DateTime(isoDate, DateTimeZone.UTC);
+            return dt.toString();
+        } catch (IllegalArgumentException e)
+        {
+            throw new AlfrescoRuntimeException("Failed to parse date " + isoDate, e);
+        }
     }
-    
+
     /**
-     * Parse date from ISO formatted string. 
+     * Parse date from ISO formatted string.
      * The ISO8601 date must include TimeZone offset information
-     * 
+     *
      * @param isoDate  ISO string to parse
      * @return  the date
      * @throws AlfrescoRuntimeException         if the parse failed
      */
     public static Date parse(String isoDate)
     {
-       return parseInternal(isoDate, null);
+        return parseInternal(isoDate, null);
     }
 
     /**
      * Parse date from ISO formatted string, with an
      *  explicit timezone specified
-     * 
+     *
      * @param isoDate  ISO string to parse
      * @param timezone The TimeZone the date is in
      * @return  the date
@@ -192,13 +193,13 @@ public class ISO8601DateFormat
      */
     public static Date parse(String isoDate, TimeZone timezone)
     {
-       return parseInternal(isoDate, timezone);
+        return parseInternal(isoDate, timezone);
     }
-    
+
     /**
      * Parse date from ISO formatted string, either in the specified
      *  TimeZone, or with TimeZone information taken from the date
-     * 
+     *
      * @param isoDate  ISO string to parse
      * @param timezone The time zone, null means default time zone
      * @return  the date
@@ -206,83 +207,98 @@ public class ISO8601DateFormat
      */
     public static Date parseInternal(String isoDate, TimeZone timezone)
     {
-		try 
-		{
-			// null time-zone defaults to the local time-zone
-			DateTimeZone dtz = DateTimeZone.forTimeZone(timezone);
-			DateTime dateTime = new DateTime(isoDate, dtz);
-			Date date = dateTime.toDate();
-			return date;
-		} 
-		catch (IllegalArgumentException e) 
-		{
-			throw new AlfrescoRuntimeException("Failed to parse date " + isoDate, e);
-		}
+        try
+        {
+            // null time-zone defaults to the local time-zone
+            DateTimeZone dtz = DateTimeZone.forTimeZone(timezone);
+            try
+            {
+                Chronology chrono = GJChronology.getInstance(dtz);
+                DateTime dateTime = new DateTime(isoDate, chrono);
+                Date date = dateTime.toDate();
+                return date;
+            }
+            catch (IllegalInstantException ie)
+            {
+                // The exception is thrown when a DateTime was created with a date-time inside the DST gap - a time that did not exist.
+                // Parse the date ignoring the time.
+                DateTimeFormatter parser = ISODateTimeFormat.dateTimeParser();
+                LocalDate ldate = new LocalDate(parser.parseLocalDate(isoDate), dtz);
+                // Default to the first valid date-time of the day, not always 00:00 (because of DST).
+                DateTime dateT = ldate.toDateTimeAtStartOfDay(dtz);
+                Date date = dateT.toDate();
+                return date;
+            }
+        }
+        catch (IllegalArgumentException e)
+        {
+            throw new AlfrescoRuntimeException("Failed to parse date " + isoDate, e);
+        }
     }
-    
+
     /**
      * Checks whether or not the given ISO8601-formatted date-string contains a time-component
      * instead of only the actual date.
-     * 
+     *
      * @param isoDate
      * @return true, if time is present.
      */
     public static boolean isTimeComponentDefined(String isoDate)
     {
-    	boolean defined = false;
-    	
-    	if(isoDate != null && isoDate.length() > 11) 
-    	{
-    		// Find occurrence of T (sYYYY-MM-DDT..), sign is optional
-    		int expectedLocation = 10;
-    		if(isoDate.charAt(0) == '-' || isoDate.charAt(0) == '+') {
-    			// Sign is included before year
-    			expectedLocation++;
-    		}
-    		
-    		defined = isoDate.length() >= expectedLocation && isoDate.charAt(expectedLocation) == 'T';
-    	}
-    	
-    	return defined;
+        boolean defined = false;
+
+        if(isoDate != null && isoDate.length() > 11)
+        {
+            // Find occurrence of T (sYYYY-MM-DDT..), sign is optional
+            int expectedLocation = 10;
+            if(isoDate.charAt(0) == '-' || isoDate.charAt(0) == '+') {
+                // Sign is included before year
+                expectedLocation++;
+            }
+
+            defined = isoDate.length() >= expectedLocation && isoDate.charAt(expectedLocation) == 'T';
+        }
+
+        return defined;
     }
-    
+
     /**
      * Parses the given ISO8601-formatted date-string, not taking into account the time-component.
      * The time-information for the will be reset to zero.
-     * 
+     *
      * @param isoDate the day (formatted sYYYY-MM-DD) or a full date (sYYYY-MM-DDThh:mm:ss.sssTZD)
      * @param timezone the timezone to use
      * @return the parsed date
-     * 
+     *
      * @throws AlfrescoRuntimeException if the parsing failed.
      */
     public static Date parseDayOnly(String isoDate, TimeZone timezone)
     {
-    	try
-    	{
-    		if(isoDate != null && isoDate.length() >= 10) 
-        	{   
-        		int offset = 0;
-        		
-        		// Sign can be included before year
-        		boolean bc = false;
-        		if(isoDate.charAt(0) == '-')
-        		{
-        			bc = true;
-        			offset++;
-        		}
-        		else if(isoDate.charAt(0) == '+')
-        		{
-        			offset++;
-        		}
-        		
-        		// Extract year
+        try
+        {
+            if(isoDate != null && isoDate.length() >= 10)
+            {
+                int offset = 0;
+
+                // Sign can be included before year
+                boolean bc = false;
+                if(isoDate.charAt(0) == '-')
+                {
+                    bc = true;
+                    offset++;
+                }
+                else if(isoDate.charAt(0) == '+')
+                {
+                    offset++;
+                }
+
+                // Extract year
                 int year = Integer.parseInt(isoDate.substring(offset, offset += 4));
                 if (isoDate.charAt(offset) != '-')
                 {
                     throw new IndexOutOfBoundsException("Expected - character but found " + isoDate.charAt(offset));
                 }
-                
+
                 // Extract month
                 int month = Integer.parseInt(isoDate.substring(offset += 1, offset += 2));
                 if (isoDate.charAt(offset) != '-')
@@ -292,7 +308,7 @@ public class ISO8601DateFormat
 
                 // Extract day
                 int day = Integer.parseInt(isoDate.substring(offset += 1, offset += 2));
-                
+
                 Calendar calendar = new GregorianCalendar(timezone);
                 calendar.setLenient(false);
                 calendar.set(Calendar.YEAR, year);
@@ -304,17 +320,17 @@ public class ISO8601DateFormat
                 calendar.set(Calendar.MILLISECOND, 0);
                 if(bc)
                 {
-                	calendar.set(Calendar.ERA, GregorianCalendar.BC);
+                    calendar.set(Calendar.ERA, GregorianCalendar.BC);
                 }
-                
+
                 return calendar.getTime();
-        	}
-    		else
-    		{
-    			throw new AlfrescoRuntimeException("String passed is too short " + isoDate);
-    		}
-    	}
-    	catch(IndexOutOfBoundsException e)
+            }
+            else
+            {
+                throw new AlfrescoRuntimeException("String passed is too short " + isoDate);
+            }
+        }
+        catch(IndexOutOfBoundsException e)
         {
             throw new AlfrescoRuntimeException("Failed to parse date " + isoDate, e);
         }
@@ -323,6 +339,6 @@ public class ISO8601DateFormat
             throw new AlfrescoRuntimeException("Failed to parse date " + isoDate, e);
         }
     }
-        
-    
+
+
 }
